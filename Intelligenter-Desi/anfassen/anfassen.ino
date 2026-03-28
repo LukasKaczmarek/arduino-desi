598b24ed59b9ee13cce85bd34eab0812eb8bce28
Risandu Bometh Bothala Ralalage <bothalar@hs-pforzheim.de>
2026-03-28 22:20:11 +0100

    finale ohne protokoll

--- Intelligenter-Desi/anfassen/anfassen.ino
+++ Intelligenter-Desi/anfassen/anfassen.ino
@@ -1,129 +1,164 @@
+#include <Wire.h>
+#include <RTClib.h>
 #include <SPI.h>
 #include <MFRC522.h>
 
-const float soundSpeedCm = 0.034;
-const int timeFor15ml = 3000;
-const int handThreshold = 15;
-
+// -------------------- Ultraschall --------------------
 class UltrasonicSensor {
-  private:
-    int trigPin;
-    int echoPin;
-    bool movement = false;
-    int distance = 0;
+  int trigPin, echoPin;
 
-  public:
-    UltrasonicSensor(int trig, int echo){
-      trigPin = trig;
-      echoPin = echo;
-    }
+public:
+  UltrasonicSensor(int trig, int echo) {
+    trigPin = trig;
+    echoPin = echo;
+  }
 
-    bool getMovement() {
-      return movement;
-    }
+  void begin() {
+    pinMode(trigPin, OUTPUT);
+    pinMode(echoPin, INPUT);
+  }
 
-    int getTrigPin() {
-      return trigPin;
-    }
+  long getDistance() {
+    digitalWrite(trigPin, LOW);
+    delayMicroseconds(2);
+    digitalWrite(trigPin, HIGH);
+    delayMicroseconds(10);
+    digitalWrite(trigPin, LOW);
 
-    void setMovement(bool value) {
-      movement = value;
-    }
-
-    void start(){
-      pinMode(trigPin, OUTPUT);
-      pinMode(echoPin, INPUT);
-      digitalWrite(trigPin, LOW);
-    }
-
-    int measureDistance() {
-      long duration;
-      digitalWrite(trigPin, LOW);
-      delayMicroseconds(2);
-      digitalWrite(trigPin, HIGH);
-      delayMicroseconds(10);
-      digitalWrite(trigPin, LOW);
-
-      duration = pulseIn(echoPin, HIGH);
-      int dist = duration * soundSpeedCm / 2;
-      return dist;
-    }
-
-    bool isHandDetected() {
-      float distance = measureDistance();
-      Serial.print("Distance: ");
-      Serial.println(distance);
-      return (distance > 0 && distance <= handThreshold);
-    }
+    long duration = pulseIn(echoPin, HIGH);
+    long distance = duration * 0.034 / 2;
+    return distance;
+  }
 };
 
-class Waterpump {
-  private:
-    int pin;
+// -------------------- Pumpe --------------------
+class Pump {
+  int pin;
+  bool active = false;
+  unsigned long startTime = 0;
+  unsigned long duration = 1000;
 
-  public:
-    Waterpump(int p){
-      pin = p;
-    }
+public:
+  Pump(int p) { pin = p; }
 
-    void start(){
-      pinMode(pin, OUTPUT);
-      digitalWrite(pin, LOW);
-    }
+  void begin() {
+    pinMode(pin, OUTPUT);
+    digitalWrite(pin, LOW);
+  }
 
-    void turnOn() {
-      digitalWrite(pin, HIGH);
-    }
+  void activate() {
+    digitalWrite(pin, HIGH);
+    Serial.println("Pumpe AN");
+    delay(1000);
+    digitalWrite(pin, LOW);
+    Serial.println("Pumpe AUS");
+  }
 
-    void turnOff() {
-      digitalWrite(pin, LOW);
-    }
-
-    void pulse(int ms){
-      turnOn();
-      delay(ms);
-      turnOff();
-    }
+ 
 };
 
-class RFIDReader {
-  private:
-  MFRC522 mfrc;
+// -------------------- RTC --------------------
+class RTCModule {
+  RTC_DS3231 rtc;
 
-  public:
-  RFIDReader(int ssPin, int rstPin) : mfrc(ssPin, rstPin){}
-
-  void start (){
-    SPI.begin();
-    mfrc.PCD_Init();
-    Serial.println("RFID bereit");
-  }
-  long readI(){
-    if(!mfrc.PICC_IsNewCardPresent()){
-      return 0;
-    }
-    if(!mfrc.PICC_ReadCardSerial()){
-      return 0;
-    }
-  }
+public:
+  void begin() { rtc.begin();
 }
 
-UltrasonicSensor usSensor(9, 10);
-Waterpump pump(8);
+  String getTime() {
+    
+    DateTime now = rtc.now();
+    int hourDE = now.hour() + 1; 
+    if (hourDE >= 24) hourDE -= 24;
+    String t = "";
+    t += String(now.day()) + ".";
+    t += String(now.month()) + ".";
+    t += String(now.year()) + " ";
+    t += String(now.hour()) + ":";
+    t += String(now.minute()) + ":";
+    t += String(now.second());
+    return t;
+  }
+};
 
+// -------------------- RFID --------------------
+class RFIDModule {
+public:
+  MFRC522 rfid;
+
+  RFIDModule(int ss, int rst) : rfid(ss, rst) {}
+
+  void begin() {
+    SPI.begin();
+    rfid.PCD_Init();
+  }
+
+  bool isNewCardPresent() {
+    return rfid.PICC_IsNewCardPresent() && rfid.PICC_ReadCardSerial();
+  }
+
+  String getUID() {
+    String uid = "";
+    for (byte i = 0; i < rfid.uid.size; i++) {
+      if (rfid.uid.uidByte[i] < 0x10) uid += "0"; // führende Null
+      uid += String(rfid.uid.uidByte[i], HEX);
+    }
+    uid.toUpperCase();
+    return uid;
+  }
+};
+
+// -------------------- OBJEKTE --------------------
+UltrasonicSensor sensor(7, 6);
+Pump pump(5);
+RTCModule rtcModule;
+RFIDModule rfidModule(10, 9);
+
+// -------------------- VARIABLEN --------------------
+long threshold = 10;
+String currentUID = "";
+bool cardPresent = false;
+
+// -------------------- SETUP --------------------
 void setup() {
   Serial.begin(9600);
-  usSensor.start();
-  pump.start();
-  Serial.println("Start");
+  sensor.begin();
+  pump.begin();
+  rtcModule.begin();
+  rfidModule.begin();
+  Serial.println("System gestartet...");
 }
 
+// -------------------- LOOP --------------------
 void loop() {
-   if (usSensor.isHandDetected()) {
-    Serial.println("Hand erkannt! Pumpe läuft...");
-    pump.pulse(timeFor15ml);
-    delay(3000);
-  } else {
-    Serial.println("Keine Hand");
+  long distance = sensor.getDistance();
+  Serial.println("Distanz: " + String(distance) + " cm");
+
+  // -------- RFID prüfen --------
+  if (rfidModule.isNewCardPresent()) {
+    currentUID = rfidModule.getUID();
+    cardPresent = true;
   }
+
+  // -------- Pumpen-Logik --------
+  if (distance > 0 && distance < threshold) {
+    String timeDE = rtcModule.getTime();
+    if (cardPresent) {
+      Serial.println("Personal erkannt: " + currentUID);
+      Serial.println("Zeit: " + timeDE);
+    } else {
+      Serial.println("Patient erkannt");
+    }
+
+    pump.activate();
+    
+
+  // -------- Karte weg? --------
+  if (!rfidModule.rfid.PICC_IsNewCardPresent() && cardPresent) {
+    cardPresent = false;
+    currentUID = "";
+  }
+
+  delay(4000); // kurze Pause für Loop
+}
 }
